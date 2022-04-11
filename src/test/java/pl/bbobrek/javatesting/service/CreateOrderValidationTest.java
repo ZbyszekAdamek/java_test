2c143b91cdd80b10657e6807ad772fe92f563cd7
ZbyszekAdamek <zbigniew.t.adamek@gmail.com>
2022-04-11 16:04:32 +0200

    wykonane testy do CreateOrderValidation

--- src/test/java/pl/bbobrek/javatesting/service/CreateOrderValidationTest.java
+++ src/test/java/pl/bbobrek/javatesting/service/CreateOrderValidationTest.java
@@ -12,16 +12,35 @@ import static org.junit.jupiter.api.Assertions.*;
 class CreateOrderValidationTest {
 
     @Test
-    public void shouldReturnFalseIfCreateOrderDtoListOfProductsIsEmpty(){
-
+    public void shouldReturnTrueIfCreatedOrderDtoListOfProductsIsNotNullOrEmpty(){
         //given
-/*        CreateOrderDto.ProductDto productDto = new CreateOrderDto.ProductDto();
+        CreateOrderDto.ProductDto productDto = new CreateOrderDto.ProductDto();
         productDto.setId(1);
         productDto.setQuantity(5);
 
         CreateOrderDto.ProductDto productDto1 = new CreateOrderDto.ProductDto();
         productDto1.setId(2);
-        productDto1.setQuantity(4);*/
+        productDto1.setQuantity(4);
+
+        CreateOrderDto createOrderDto = new CreateOrderDto();
+        List<CreateOrderDto.ProductDto>productDtos = new ArrayList<>();
+        productDtos.add(productDto);
+        productDtos.add(productDto1);
+        createOrderDto.setProducts(productDtos);
+
+        //when
+        boolean result = CreateOrderValidation.validateCreateOrderDto(createOrderDto);
+
+        //then
+        assertTrue(result);
+
+    }
+
+    @Test
+    public void shouldReturnFalseIfCreateOrderDtoListOfProductsIsEmpty(){
+
+        //given
+
 
         CreateOrderDto createOrderDto = new CreateOrderDto();
         List<CreateOrderDto.ProductDto>productDtos = new ArrayList<>();
@@ -59,20 +78,77 @@ class CreateOrderValidationTest {
         productDto.setQuantity(5);
 
         CreateOrderDto.ProductDto productDto1 = new CreateOrderDto.ProductDto();
-        productDto1.setId(2);
+        productDto1.setId(0);
         productDto1.setQuantity(4);
 
         CreateOrderDto createOrderDto = new CreateOrderDto();
         List<CreateOrderDto.ProductDto>productDtos = new ArrayList<>();
+        productDtos.add(productDto);
+        productDtos.add(productDto1);
         createOrderDto.setProducts(productDtos);
 
 
         //when
         boolean result = productDtos.
-                stream().anyMatch(i -> i.getId() == 0);
+                stream().allMatch(i -> i.getId() != 0);
 
         //then
         assertFalse(result);
     }
 
+    @Test
+    public void shouldReturnFalseIfQuantityOfAnyProductDtoIsZero(){
+
+        //given
+        CreateOrderDto.ProductDto productDto = new CreateOrderDto.ProductDto();
+        productDto.setId(1);
+        productDto.setQuantity(5);
+
+        CreateOrderDto.ProductDto productDto1 = new CreateOrderDto.ProductDto();
+        productDto1.setId(1);
+        productDto1.setQuantity(0);
+
+        CreateOrderDto createOrderDto = new CreateOrderDto();
+        List<CreateOrderDto.ProductDto>productDtos = new ArrayList<>();
+        productDtos.add(productDto);
+        productDtos.add(productDto1);
+        createOrderDto.setProducts(productDtos);
+
+
+        //when
+        boolean result = productDtos.
+                stream().allMatch(i -> i.getQuantity() != 0);
+
+
+        //then
+        assertFalse(result);
+    }
+
+    @Test
+    public void shouldReturnTrueIfAllCreateProductDtoMatchesValidation(){
+        //given
+        CreateOrderDto.ProductDto productDto = new CreateOrderDto.ProductDto();
+        productDto.setId(1);
+        productDto.setQuantity(5);
+
+        CreateOrderDto.ProductDto productDto1 = new CreateOrderDto.ProductDto();
+        productDto1.setId(1);
+        productDto1.setQuantity(2);
+
+        CreateOrderDto createOrderDto = new CreateOrderDto();
+        List<CreateOrderDto.ProductDto>productDtos = new ArrayList<>();
+        productDtos.add(productDto);
+        productDtos.add(productDto1);
+        createOrderDto.setProducts(productDtos);
+
+
+        //when
+        boolean result = productDtos.
+                stream().allMatch(i -> i.getQuantity() != 0
+                & i.getId() != 0);
+
+
+        //then
+        assertTrue(result);
+    }
 }
